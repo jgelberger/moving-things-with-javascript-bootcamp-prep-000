3d0a0cdfc968079f29eebe26551a58574b005c1b
Joshua Gelberger <joshua_gelberger@alumni.brown.edu>
2018-09-18 02:44:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,10 +20,10 @@ function moveDodgerLeft() {
 }
 
 function moveDodgerRight() {
-  var rightNumbers = dodger.style.right.replace('px', '');
-  var right = parseInt(rightNumbers, 10);
+  var rightNumbers = dodger.style.left.replace('px', '');
+  var right = parseInt(rightNumbers, 10) + 40;
   
   if (right < 400) {
-    dodger.style.right = `${right + 1}px`;
+    dodger.style.left = `${right + 1}px`;
   }
 }
