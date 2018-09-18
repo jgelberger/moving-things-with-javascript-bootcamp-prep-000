40762edfd2f8c4a092a29975daecc98e5267b381
Joshua Gelberger <joshua_gelberger@alumni.brown.edu>
2018-09-18 02:43:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function moveDodgerLeft() {
   var left = parseInt(leftNumbers, 10)
  
   if (left > 0) {
-    dodger.style.right = `${left - 1}px`
+    dodger.style.left = `${left - 1}px`
   }
 }
 
